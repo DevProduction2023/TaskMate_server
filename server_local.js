250b1bff27c4cba0ffa7f5f7285406cb547ae9ae
DevProduction2023 <shivcom2020@gmail.com>
2025-07-09 11:40:04 +0530

    Add files via upload

---
+++ server_local.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const connectDB = require('./src/utils/db/config');
+const cookieParser = require('cookie-parser');
+const userRouter = require('./src/routes/authRouter')
+const listRouter = require('./src/routes/ListOprations');
+const cors = require('cors');
+
+dotenv.config();
+connectDB();
+
+const app = express();
+app.use(express.json());
+app.use(cookieParser());
+
+app.use(cors({
+    origin: "http://localhost:5173", 
+    credentials: true,
+}));
+
+app.use('/api/auth', userRouter);
+app.use('/api/list', listRouter);
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
